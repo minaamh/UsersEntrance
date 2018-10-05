b62e97d63485495a35e64cd3cdc53588cbf4bf99
Mina <mina@openroad.ca>
2018-10-04 17:03:31 -0700

    Starting the project

---
+++ UsersEntrance/UsersEntrance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace UsersEntrance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u1 = new User();
+            Console.WriteLine("Please enter your First Name");
+            u1.Firstname = Console.ReadLine().ToUpper();
+            Console.WriteLine("Please enter your Last Name");
+            u1.Lastname = Console.ReadLine().ToUpper();
+
+            string query = $"SELECT TOP 1 [UserID]  FROM [UsersEntrance].[dbo].[UsersEntrance] where [FirstName] = '{u1.Firstname}' and [LastName] = '{u1.Lastname}'";
+            string connectionString = "server=Localhost;Trusted_Connection=true";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                u1.UserID = connection.Query<int>(query).ElementAt(0);
+
+                Console.WriteLine(u1.UserID);
+
+            }
+        }
+    }
+}
---
+++ UsersEntrance/UsersEntrance/User.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersEntrance
+{
+    class User
+    {
+        public User()
+        {
+     
+        }
+
+        public User(int userID, string firstName, string lastName, int gate)
+        {
+            _userID = userID;
+            _firstName = firstName;
+            _lastName = lastName;
+            _gate = gate;
+        }
+
+        public int UserID
+        {
+            get
+            {
+                return _userID;
+            }
+
+            set
+            {
+                _userID = value;
+            }
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return _firstName;
+            }
+
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        private int _userID;
+        private string _firstName;
+        private string _lastName;
+        private int _gate;
+    }
+}
